cd2cf5eb65d90b08cf3ee008cec24812c5da8264
Tiago Luiz Fernandes <tiago.luiz@gft.com>
2022-01-27 17:44:07 -0300

    versao inicial

---
+++ src/test/java/br/com/tts/diopersonapi/DioPersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tts.diopersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
