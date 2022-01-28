131e290c131dd889636aea3b14ef23a0c9be49b1
Tiago Luiz Fernandes <tiago.luiz@gft.com>
2022-01-28 18:47:09 -0300

    Adicionado find by id e findAll

--- src/main/java/br/com/tts/diopersonapi/controller/PersonController.java
+++ src/main/java/br/com/tts/diopersonapi/controller/PersonController.java
@@ -2,16 +2,21 @@ package br.com.tts.diopersonapi.controller;
 
 import br.com.tts.diopersonapi.dto.MessageResponseDto;
 import br.com.tts.diopersonapi.dto.request.PersonDTO;
+import br.com.tts.diopersonapi.exception.PersonNotFoundException;
 import br.com.tts.diopersonapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/people")
@@ -21,6 +26,7 @@ public class PersonController {
 
     @Autowired
     public PersonController(PersonService personService){
+
         this.personService = personService;
     }
 
@@ -28,6 +34,18 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDto createPerson(@RequestBody @Valid PersonDTO personDto) {
         return personService.createPerson(personDto);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+
+        return personService.listAll();
+
+    }
+
+    @GetMapping(path = "{id}")
+    public PersonDTO findById(@PathVariable("id") Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 
 }
--- src/main/java/br/com/tts/diopersonapi/service/PersonService.java
+++ src/main/java/br/com/tts/diopersonapi/service/PersonService.java
@@ -3,10 +3,15 @@ package br.com.tts.diopersonapi.service;
 import br.com.tts.diopersonapi.dto.MessageResponseDto;
 import br.com.tts.diopersonapi.dto.request.PersonDTO;
 import br.com.tts.diopersonapi.entity.Person;
+import br.com.tts.diopersonapi.exception.PersonNotFoundException;
 import br.com.tts.diopersonapi.mapper.PersonMapper;
 import br.com.tts.diopersonapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -31,4 +36,17 @@ public class PersonService {
                 .message("Created person with ID " + createPerson.getId())
                 .build();
     }
+
+    public List<PersonDTO> listAll() {
+        List<Person> people = personRepository.findAll();
+        return people.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+
+        Person Person = personRepository.findById(id).orElseThrow(throw new PersonNotFoundException(id));
+
+        return personMapper.toDTO(Person);
+    }
+
 }
